f7fca0e9553c3d8de6138d4b012ba93c6e95c02d
dustin george <DustinG27@gmail.com>
2021-06-17 22:20:44 -0400

    computer word shows up with a underscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,13 @@
+
 // global variables to hold basic game structure
 var bands = ["decendents", "misfits", "fuagzi", "jfa", "adolescents", "circle jerks"];
+var words = [];
+var guessedLetters = [];
+var userWins = 0;
+var userLose = 0;
 
 // variables getting id from html
-var word = document.getElementById("word");
+
 var userGuess = document.getElementById("userGuess");
 var guessedLetters = document.getElementById("guessedLetters");
 var userWins = document.getElementById("userWins");
@@ -12,6 +17,29 @@ var userLose = document.getElementById("userLose");
 var randomBand = bands[Math.floor(Math.random()*bands.length)];
 console.log(randomBand);
 
+//take bands and replace with "_"
+for (var i = 0; i < randomBand.length; i++) {
+   words[i] = "_";
+   console.log(words);
+   document.getElementById("word").innerHTML = words.join(" ");
+}
+
+var remainingLetters = randomBand.length;
+// onletter key up code
+document.onkeyup = function(event){
+// creating the event for user keyup
+    var letter = event.key.toLowerCase();
+    
 // capture user innput with keyup
+    console.log(letter);
 
 //check userinput with letters in array
+
+// statement for guessing a word
+
+// for loop for replacing letters in random bands
+
+
+
+}
+
